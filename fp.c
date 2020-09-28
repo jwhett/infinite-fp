e9e356f1d7e38f172d35c78dfe6a194680dd840e
Josh Whetton <josh@whetton.net>
2020-09-28 17:08:12 -0400

    Change logic slightly.

--- fp.c
+++ fp.c
@@ -5,34 +5,39 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define		UP	0
 #define		LEFT	1
 #define		MIDDLE	2
 #define		RIGHT	4
 
+#define		F4	KEY_F4
+#define		F5	KEY_F5
+#define		F6	KEY_F6
 
 int main(int argc, char** argv)
 {
-    // TODO: compare bytes in buff to ID state
-
     if (argc < 2)
         exit(1);
 
     char *buff;
     buff = calloc(2, sizeof(char));
 
-    int handle = open(argv[1], O_RDONLY); // open file readonly
+    // open file readonly
+    int handle = open(argv[1], O_RDONLY);
     if (handle < 0)
         perror("OPEN");
 
-    while (1)
+    while (read(handle, buff, 2))
     {
-        read(handle, buff, 2); // read 2 bytes into buff
+        // read 2 bytes into buff, but we only
+        // care about the first.
         if (buff[0] & LEFT)
             printf("LEFT\n");
-        if (buff[0] & MIDDLE)
+        else if (buff[0] & MIDDLE)
             printf("MIDDLE\n");
-        if (buff[0] & RIGHT)
+        else if (buff[0] & RIGHT)
             printf("RIGHT\n");
-       // printf("Buff: %x\n", buff[0]);
+        else if (buff[0] & UP)
+            printf("UP\n");
     }
 }
